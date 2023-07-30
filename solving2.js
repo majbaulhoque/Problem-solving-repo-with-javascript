d1a4b5ab6733705b130108492cef7c6158b87d9d
majba-ul-hoque <majba9399@gmail.com>
2023-07-30 16:12:15 +0600

    Average marks Calculation

---
+++ solving2.js
@@ -0,0 +1,11 @@
+var bangla = 75.25;
+var english = 65;
+var math = 80;
+var chemistry = 34.45;
+var physics = 99.50;
+
+var sum = bangla + english + math + chemistry + physics;
+
+var average = sum / 5
+
+console.log(average);
