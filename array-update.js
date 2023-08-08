44f041549e6878e4a78982e9d769666840c0db96
majba-ul-hoque <majba9399@gmail.com>
2023-08-08 17:37:49 +0600

    index find and push pop problem

---
+++ array-update.js
@@ -0,0 +1,12 @@
+var fruits = ['Apple','Banana','orange'];
+
+var bananaIndex = fruits.indexOf('Banana');
+fruits[1] = 'Mango';
+console.log(fruits);
+
+// Remove part 
+
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
---
+++ solving3.js
@@ -0,0 +1,4 @@
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer.';
+var finalLine = firstLine +' '+secondLine;
+console.log(finalLine);
