6177344612ce8cf58f5bb626ef45873aeab21826
majba-ul-hoque <majba9399@gmail.com>
2023-08-13 22:32:22 +0600

    using function finding max number

---
+++ max.js
@@ -0,0 +1,29 @@
+// const jim = 84;
+// const dela = 99;
+// const chinku = 77;
+
+// // if(jim > dela){
+// //     console.log('Jim will get the cake');
+// // }else{
+// //     console.log('Dela you will get the cake');
+// // }
+
+// if(jim > dela && jim > chinku){
+//     console.log('Jim will get the cake');
+// }else if(dela > jim && dela > chinku){
+//     console.log('Dela will get the cake');
+// }else{
+//     console.log('Chinku will get the cake');
+// }
+
+function maxOfThree(a, b, c){
+    return Math.max(a, b, c);
+}
+
+const num1 = 77;
+const num2 = 56;
+const num3 = 78;
+
+const result = maxOfThree(num1, num2, num3);
+console.log("The maximum number is : ", result)
+
